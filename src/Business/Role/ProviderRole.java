82fc14369765e0ca5d730dcb20b106c48df69b12
swini25 <rodrigues.sw@northeastern.edu>
2022-12-03 11:50:18 -0500

    Manufacturer, Patient Roles

---
+++ src/Business/Role/ProviderRole.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Enterprise.HospitalEnterprise;
+import Business.Network.StateNetwork;
+import Business.Organization.HospitalOrganization;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import UserInterface.Provider.ProviderWorkAreaJPanel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author swinirodrigues
+ */
+public class ProviderRole extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Enterprise enterprise, Organization organization, EcoSystem business, StateNetwork state) {
+        return new ProviderWorkAreaJPanel(userProcessContainer, account, (HospitalEnterprise)enterprise, (HospitalOrganization)organization, business, state);
+    }
+    
+}
