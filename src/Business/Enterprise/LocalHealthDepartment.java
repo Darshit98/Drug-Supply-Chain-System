85b2ee5e12423696e0d4c3c3a27643ea78977c6d
swini25 <rodrigues.sw@northeastern.edu>
2022-12-04 02:47:32 -0500

    Enterprise

---
+++ src/Business/Enterprise/LocalHealthDepartment.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Enterprise;
+
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author swinirodrigues
+ */
+public class LocalHealthDepartment extends Enterprise {
+
+    public LocalHealthDepartment(String stateName) {
+        super(stateName, EnterpriseType.LocalHealthDepartment);
+    }
+
+    
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
+}
