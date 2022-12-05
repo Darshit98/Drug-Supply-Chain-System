49032a91e15ec4ce1d8bd2e7b1f464de34586498
swini25 <rodrigues.sw@northeastern.edu>
2022-12-05 02:41:56 -0500

    NationalEnterprise

---
+++ src/Business/NationalEnterprise/CDC.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Business.NationalEnterprise;
+
+import Business.Enterprise.Enterprise;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author swinirodrigues
+ */
+
+public class CDC extends Enterprise{
+
+    public CDC(String name) {
+        super(name, EnterpriseType.CDC);
+    }
+
+    
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+        
+    }
+    
+}
